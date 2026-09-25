fe08ccfe9c19e03c24d507826a411ae30417cc25
Felipe Gonzalez <felipe.gonzalez@duocuc.cl>
2026-09-25 17:58:31 -0300

    feat: se personaliza el contenido inicial

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 const botonBienvenida = document.querySelector("#boton-bienvenida");
 
 botonBienvenida.addEventListener("click", () => {
-  alert("Durante el semestre construiremos Conecta Cultura paso a paso.");
+  alert("Durante el semestre construiremos Conecta Cultura paso a paso. No te quedes atrás y participa.");
 });
